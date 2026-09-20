d02cbef3b35e20acc76503b93e042301633d463a
Raghavendra Singh <raghavendrashivam474@gmail.com>
2026-09-20 17:22:19 +0530

    test(s2): establish reusable WorldStore contract test suite and persistence error hierarchy

---
+++ src/persistence/errors/persistence.error.ts
@@ -0,0 +1,41 @@
+﻿export class PersistenceError extends Error {
+  constructor(message: string, public readonly cause?: unknown) {
+    super(message);
+    this.name = "PersistenceError";
+  }
+}
+
+export class InitializationError extends PersistenceError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "InitializationError";
+  }
+}
+
+export class ReadError extends PersistenceError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "ReadError";
+  }
+}
+
+export class WriteError extends PersistenceError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "WriteError";
+  }
+}
+
+export class PersistenceValidationError extends PersistenceError {
+  constructor(message: string, cause?: unknown) {
+    super(message, cause);
+    this.name = "PersistenceValidationError";
+  }
+}
+
+export class UnsupportedSchemaVersionError extends PersistenceError {
+  constructor(public readonly version: number, message?: string) {
+    super(message ?? `Unsupported schema version: ${version}`);
+    this.name = "UnsupportedSchemaVersionError";
+  }
+}
--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -1,2 +1,3 @@
 ﻿export * from "./ports/world-store.port.js";
 export * from "./memory/in-memory-world-store.js";
+export * from "./errors/persistence.error.js";
